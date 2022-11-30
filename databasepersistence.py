de7b7b8ecac514bb5b7fe4fe6c699e7255a8e32f
Aske Møller Jørgensen <askemj@gmail.com>
2022-11-30 19:37:34 +0100

    set test for _find_items_to_delete

--- databasepersistence.py
+++ databasepersistence.py
@@ -247,6 +247,10 @@ if __name__ == "__main__":
     new_tag = Tag('Lækkert')
     new_ingredient = Ingredient(2,'dl','chokoladesauce','Tørvarer','hovedingrediens',False)
     new_ingredient2 = Ingredient(3,'kugle(r)','is','Frost','hovedingrediens',False)
+    new_ingredient3 = Ingredient(2,'spsk','frysetørret hindbær','Tørvarer','hovedingrediens',False)
     new_recipe = Recipe('is og chokolade', 'Tilberedes lige inden servering', 2, 10, 15, 'Dessert', [new_tag], [new_ingredient, new_ingredient2])
+    new_recipe2 = Recipe('is og chokolade', 'Tilberedes lige inden servering', 2, 10, 15, 'Dessert', [new_tag], [new_ingredient3, new_ingredient2])
     new_recipe.ID=7
-    database.insert_recipe(new_recipe)
+    #database.insert_recipe(new_recipe)
+    
+    print( database._find_items_for_deletion(new_recipe2, new_recipe)[0][0].name )
