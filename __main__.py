4b06274127ae604e158d4253214e440d932f5e27
askemj <askemj@gmail.com>
2023-02-04 15:08:35 +0100

    updated MVC structure

--- __main__.py
+++ __main__.py
@@ -1,15 +1,18 @@
 from pyqtrecipeeditorcontroller import PyQtRecipeEditorController
-from PyQtRecipeEditorUI import Ui_MainWindow
+from PyQt_recipe_editor_mainwindow import Ui_MainWindow
+from pyqt_recipe_editor_view import RecipeEditorView
 from PyQt5 import QtCore, QtGui, QtWidgets
+import time 
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
+    ui = RecipeEditorView(MainWindow) #Ui_MainWindow()
+    #ui.setupUi(MainWindow)
     MainWindow.show()
 
     PyQtRecEdt = PyQtRecipeEditorController(database = [], view=ui)
 
-    sys.exit(app.exec_())
+
+    sys.exit(app.exec_()) 
