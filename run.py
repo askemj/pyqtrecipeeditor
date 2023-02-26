38aa1a339a327922ead426257bcad7d54a8a2bde
askemj <askemj@gmail.com>
2023-02-26 12:55:24 +0100

    Updated and renamed __main__ to run.py

--- run.py
+++ run.py
@@ -1,18 +1,22 @@
 from pyqtrecipeeditorcontroller import PyQtRecipeEditorController
+from databasepersistence import DatabasePersistence
 from PyQt_recipe_editor_mainwindow import Ui_MainWindow
 from pyqt_recipe_editor_view import RecipeEditorView
 from PyQt5 import QtCore, QtGui, QtWidgets
 import time 
 
-if __name__ == "__main__":
+def main():
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = RecipeEditorView(MainWindow) #Ui_MainWindow()
     #ui.setupUi(MainWindow)
     MainWindow.show()
-
-    PyQtRecEdt = PyQtRecipeEditorController(database = [], view=ui)
+    database = DatabasePersistence()
+    PyQtRecEdt = PyQtRecipeEditorController(database = database, view=ui) 
 
 
     sys.exit(app.exec_()) 
+
+if __name__ == "__main__":
+    main()
